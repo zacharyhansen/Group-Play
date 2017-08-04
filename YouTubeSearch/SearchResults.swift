c890f32751d4c4e06d2479d90cc3822347090417
Andrea Pesigan <pesiganal@cardiff.ac.uk>
2017-08-04 12:43:24 -0700

    YouTube Search. Having trouble with the getVideos method- the videos array is only altered within the closure in the call to performGetRequest, but the array is empty once the closure ends??

---
+++ YouTubeSearch/SearchResults.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResults.swift
+//  YouTubeSearch
+//
+//  Created by Paige Rox on 31/07/2017.
+//  Copyright © 2017 GoPlay. All rights reserved.
+//
+
+import Cocoa
+
+class SearchResults: UITableViewController {
+
+}
